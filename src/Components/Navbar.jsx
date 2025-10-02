677f26c2971094ac9072cbc10d0fd01665619f92
Franco <ufrancis186@gmail.com>
2025-10-02 11:55:51 +0100

    fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,18 +4,15 @@ function Navbar(){
     return(
         <div className="pr-32">
             <ul className="flex flex-row text-blue-950 gap-10 font-bold ">
-                <Link to="/">
-                     <li className="hover:text-gray-400 active:text-gray-200">Home</li>
-                </Link>
-
-                <Link to="/pastquestions">
-                     <li className="hover:text-gray-400 active:text-gray-200">Past Questions</li>
-                </Link>
-
-                <Link>
-                     <li className="hover:text-gray-400 active:text-gray-200">LOGIN/SIGNUP</li>
-                </Link>
-                
+                <li className="hover:text-gray-400 active:text-gray-200">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="hover:text-gray-400 active:text-gray-200">
+                    <Link to="/pastquestions">Past Questions</Link>
+                </li>
+                <li className="hover:text-gray-400 active:text-gray-200">
+                    <Link to="#">LOGIN/SIGNUP</Link>
+                </li>
             </ul>
         </div>
     );
