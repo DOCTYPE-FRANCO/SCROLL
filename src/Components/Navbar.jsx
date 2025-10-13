2fe7c1f2f93dbbfbf3a201d896a61d7dfbfca3f0
Franco <ufrancis186@gmail.com>
2025-10-13 19:22:43 +0100

    navbar fix , now its not ugly

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from "react";
 import { Link } from "react-router-dom";
 import { UserDataContext } from "../UserContext";
+import { Home, Book, Lock, XIcon, SmileIcon } from "lucide-react";
 
 function Navbar(){
     const [mobile, setMobile] = useState(false);
@@ -34,41 +35,49 @@ function Navbar(){
             </div>
 
             {mobile && (
-                <div className="fixed top-0 left-0 bg-gray-800 w-full h-[35vh] rounded-b-2xl flex flex-col items-center transition-all duration-700">
+                <div className="fixed top-0 left-0 bg-gray-800/95 backdrop-blur-sm w-full h-[30vh] flex flex-col transition-all duration-700">
                     <div className="flex flex-row justify-between mt-4 w-full">
-                        <p className="flex justify-center text-white font-bold text-3xl pl-4">MENU</p>
-                        <button onClick={()=> {setMobile(false)}} className="text-4xl text-white mr-4">X</button>
+                        <p className="flex justify-center text-white font-thin text-2xl pl-4">MENU</p>
+                        <button onClick={()=> {setMobile(false)}} className="text-4xl text-white mr-4"><XIcon color="#FFFFFF"/></button>
                     </div>
 
                     
-                    <ul className="flex flex-col text-blue-950 gap-4 font-bold  justify-center items-center mt-16">
-                        <Link to="/">
-                            <li onClick={()=>setMobile(false)} className="text-white active:text-gray-600 text-center border-2 border-gray-300 w-[250px] font-bold">
-                                Home
-                            </li>
-                        </Link>
+                    <div className="flex flex-row">
+                        <ul className="flex flex-col justify-start text-blue-950 gap-4 font-bold pl-10  mt-16">
+                            <Link to="/">
+                                <li onClick={()=>setMobile(false)} className="flex flex-row justify-start gap-12 items-center text-white active:text-gray-600 text-center w-[150px] font-bold">
+                                    <Home color="#FFFFFF" />
+                                    Home
+                                </li>
+                            </Link>
 
-                        <Link to={logged ? "/pastquestions": ""}>
-                            <li onClick={()=> {
-                                if (!logged) {
-                                    alert("PLEASE LOGIN SKI :)");
-                                    } else {
-                                    setMobile(false);
-                                
-                                    }
-                                }}
-                             className="text-white active:text-gray-600 text-center border-2 border-gray-300 w-[250px] font-bold">
-                                Past Questions
-                            </li>
-                        </Link>
+                            <Link to={logged ? "/pastquestions": ""}>
+                                <li onClick={()=> {
+                                    if (!logged) {
+                                        alert("PLEASE LOGIN SKI :)");
+                                        } else {
+                                        setMobile(false);
+                                    
+                                        }
+                                    }}
+                                    className="flex flex-row gap-10 justify-start items-center text-white active:text-gray-600 text-center w-[350px] font-bold">
+                                    <Book color="#FFFFFF"/>
+                                    Past Questions
+                                </li>
+                            </Link>
 
-                        <Link to="/ls">
-                            <li onClick={()=>setMobile(false)} className="text-white active:text-gray-600 text-center border-2 border-gray-300 w-[250px] font-bold">
-                                LOGIN/SIGNUP
-                            </li>
-                        </Link>
-                        
-                    </ul>
+                            <Link to="/ls">
+                                <li onClick={()=>setMobile(false)} className="flex flex-row gap-10 justify-start items-center text-white active:text-gray-600 text-center  w-[150px] font-bold">
+                                    <Lock color="#FFFFFF" />
+                                    LOGIN
+                                </li>
+                            </Link>
+                            
+                        </ul>
+                        <div className="flex justify-center items-center">
+                            <SmileIcon color="#FFFFFF" className="-ml-20" size={64}/>
+                        </div>
+                    </div>
                     
                 </div>
             )}
