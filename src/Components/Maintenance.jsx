51258c595eec8f0b615076e821ce1757b82c0e42
Franco <ufrancis186@gmail.com>
2025-11-11 21:52:29 +0100

    x

--- src/Components/Maintenance.jsx
+++ src/Components/Maintenance.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 function Maintenance() {
     return(
-        <div className="fixed inset-0 z-50 backdrop-blur-sm flex justify-center items-center">
-            SCROLL IS CURRENTLY UNDER MAINTENANCE :(
+        <div className="fixed inset-0 z-50 backdrop-blur-sm flex flex-col justify-center items-center">
+            <p className="font-extrabold text-3xl text-blue-950">
+                SCROLL IS CURRENTLY UNDER MAINTENANCE :(
+            </p>
+
+            <p className="font-extrabold ">
+                BEG FRANCO TO FIX ;)
+            </p>
         </div>
     );
 }
