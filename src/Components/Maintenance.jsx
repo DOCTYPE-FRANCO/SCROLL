1699645eaa34dda73460b640a38ed351c99ad88d
Franco <ufrancis186@gmail.com>
2025-11-11 21:49:54 +0100

    MAINTENANCE ON SCROLL

--- src/Components/Maintenance.jsx
+++ src/Components/Maintenance.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Maintenance() {
     return(
-        <div className="fixed inset-0 z-50 backdrop-blur-sm">
+        <div className="fixed inset-0 z-50 backdrop-blur-sm flex justify-center items-center">
             SCROLL IS CURRENTLY UNDER MAINTENANCE :(
         </div>
     );
