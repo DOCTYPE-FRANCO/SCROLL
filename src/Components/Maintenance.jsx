4ae89caf4e6ef1d32062e2047f4613566d324755
Franco <ufrancis186@gmail.com>
2025-11-12 19:08:05 +0100

    fixed maintenance

--- src/Components/Maintenance.jsx
+++ src/Components/Maintenance.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Maintenance() {
     return(
         <div className="fixed inset-0 z-50 backdrop-blur-sm flex flex-col justify-center gap-10 items-center">
-            <p className="font-extrabold text-3xl text-blue-950">
+            <p className="font-extrabold text-2xl text-center md:text-3xl text-blue-950 md:max-w-[800px]">
                 SCROLL IS CURRENTLY UNDER MAINTENANCE :(
             </p>
 
